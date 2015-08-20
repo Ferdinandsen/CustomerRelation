7812af7f312d91b0c3fb0e7effa88fedfc49e662
unknown <Jaje@rbhi.BWS.DK>
2015-08-20 11:29:30 +0200

    Highligt virker på main activity!

---
+++ app/src/main/java/bws/customerrelation/GUI/MainActivityListViewAdapter.java
@@ -0,0 +1,68 @@
+package bws.customerrelation.GUI;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bws.customerrelation.Model.BEClient;
+import bws.customerrelation.R;
+
+/**
+ * Created by Jaje on 20-Aug-15.
+ */
+public class MainActivityListViewAdapter extends ArrayAdapter<BEClient> {
+    ArrayList<View> _views;
+    ArrayList<Integer> _selectedItems;
+    ArrayList<BEClient> _clients;
+
+    public MainActivityListViewAdapter(Context context, int resource, ArrayList<BEClient> objects, ArrayList<Integer> selectedItems) {
+        super(context, resource, objects);
+        _clients = objects;
+        _views = new ArrayList<View>();
+        _selectedItems = selectedItems;
+    }
+
+    @Override
+    public View getView(int position, View v, ViewGroup parent) {
+        if (v == null) {
+            LayoutInflater li = (LayoutInflater) getContext().getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            v = li.inflate(R.layout.cell_main_activity, null);
+        } else {
+            int i = position - 1;
+            if (position == 0)
+                i = 0;
+            v = _views.get(i);
+        }
+
+        BEClient c = _clients.get(position);
+
+        TextView company = (TextView) v.findViewById(R.id.companyName);
+        TextView canvasAmount = (TextView) v.findViewById(R.id.canvasAmount);
+
+        if (company != null) {
+            company.setText(c.getFirstName());
+        }
+        if (company != null) {
+            canvasAmount.setText("" + c.getId());
+        }
+        for (int x : _selectedItems)
+            if (position == x) {
+                v.setBackgroundColor(Color.parseColor("#00B2EE"));
+            }
+
+        if (_views.size() <= position) {
+            _views.add(v);
+        } else {
+            _views.set(position, v);
+        }
+        return v;
+    }
+}
